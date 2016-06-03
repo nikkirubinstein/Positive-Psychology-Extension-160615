d2dfddfc05f471d8adc968e6cb1504abc1f63633
timesler <tim.esler@gmail.com>
2016-06-03 07:07:59 +0000

    updates

--- AnalysisScript.r
+++ AnalysisScript.r
@@ -1,26 +1,41 @@
-library(dplyr)
+library(plyr)
+library(ggplot2)
 
 # Read in data
 
-addictionData <- read.csv("Data/addictionData_new.csv",
-                         stringsAsFactors = F,
-                         na.strings = c("NA"," ","."))
+addictionData <- read.csv("Data/addictionData.csv",
+                          stringsAsFactors = F,
+                          na.strings = c("NA"," ","."))
 
-waves <- paste0("_T",1:9)
+waves <- c("_T1","_T2","_T3","_T4","_T5","_T6","_T7","_T8","_T9")
 
 addictionDataSplit <- list()
 
 for (i in 2:9) {
   addictionDataSplit[[i]] <- addictionData[,grepl(waves[i],names(addictionData))]
-  addictionDataSplit[[i]]$id <- addictionData[,'id']
-  addictionDataSplit[[i]]$Wave <- i
   addictionData <- addictionData[,-grep(waves[i],names(addictionData))]
+  names(addictionDataSplit[[i]]) <- gsub(waves[i],"",names(addictionDataSplit[[i]]))
+  addictionDataSplit[[i]]$Wave <- i
+  addictionDataSplit[[i]]$id <- addictionData$id
 }
 addictionDataSplit[[1]] <- addictionData
 addictionDataSplit[[1]]$Wave <- 1
+names(addictionDataSplit[[1]]) <- gsub(waves[1],"",names(addictionDataSplit[[1]]))
 
 for (i in 1:9) {
   write.csv(addictionDataSplit[[i]],
-            file = paste0("Data/addictionDataWave",i,".csv"),quote = F,row.names = F)
+            file = paste0("Data/addictionDataWave",i,".csv"),
+            row.names = F)
 }
 
+addictionData_Long <- addictionDataSplit[[1]]
+
+for (i in 2:9) {
+  addictionData_Long <- rbind.fill(addictionData_Long,addictionDataSplit[[i]])
+}
+
+persistentIds = addictionData_Long$id[addictionData_Long$Wave==9 &
+                                        !is.na(addictionData_Long$videogameaddiction)]
+
+ggplot(addictionData_Long[addictionData_Long$id %in% persistentIds,],aes(x = Wave,y = videogameaddiction))+
+  geom_line(aes(by=as.factor(id)))
--- BasicAnalysis.R
+++ BasicAnalysis.R
@@ -7,11 +7,40 @@
 ########################################################################################
 
 library(plyr)
-library(dplyr)
 
-addictionData <- NULL
+# Initialise a list to hold data from each wave
+data <- list()
 
-file <- "addictionDataWave"
+# Read in data from each wave
+for (wave in 1:9) {
+  data[[wave]] <- read.csv(paste0("Data/addictionDataWave",wave,".csv"))
+}
+
+# Some exploratory data analysis
+print("Dimensions of data for each wave...")
+for (wave in 1:9) {
+  print(paste0("Wave ",wave,": ",paste(dim(data[[wave]]),collapse = ", ")))
+}
+summary(data[[1]]) # First wave summary
+shapiro.test(data[[1]]$videogameaddiction) # Shapiro-Wilk normality test
+meanVGA <- mean(data[[1]]$videogameaddiction,
+                na.rm = T)
+medianVGA <- median(data[[1]]$videogameaddiction,
+                  na.rm = T)
+sdVGA <- sd(data[[1]]$videogameaddiction,
+            na.rm = T)
+minVGA <- min(data[[1]]$videogameaddiction,
+    na.rm = T)
+maxVGA <- max(data[[1]]$videogameaddiction,
+    na.rm = T)
+quantile(data[[1]]$videogameaddiction,
+         seq(0,1,by=0.1),
+         na.rm = T)
+
+# Identifying outlier indices
+outliers <- which((data[[1]]$videogameaddiction > (meanVGA+2*sdVGA)) |
+  (data[[1]]$videogameaddiction < (meanVGA-2*sdVGA)))
+
 for(f in 1:9){
-  addictionData <- rbind.fill(addictionData, read.csv(paste0("Data/addictionDataWave",f,".csv")))
+  dataLong <- rbind.fill(data, read.csv(paste0("Data/addictionDataWave",f,".csv")))
 }
